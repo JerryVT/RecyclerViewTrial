3143a47555930e34c83e3fcae6f019559b30c0d9
Jerry Thomas <jervthomas@gmail.com>
2021-02-02 01:44:20 +0530

    Fourth Commit : Addition of Insertion and Deletion operations

--- app/src/main/java/com/example/recyclerviewtrial/ExampleAdapter.kt
+++ app/src/main/java/com/example/recyclerviewtrial/ExampleAdapter.kt
@@ -9,12 +9,20 @@ import androidx.recyclerview.widget.RecyclerView
 
 
 class ExampleAdapter (private val exampleList: List<ExampleItem>) : RecyclerView.Adapter<ExampleAdapter.ExampleViewHolder>() {
+        //this class should extend RecyclerView.Adapter
+        //since class is nested , we have given as ExampleAdapter.ExampleViewHolder
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExampleViewHolder {
 
+        //called by RecycleView when its time to create a newer ViewHolder
        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.recycler_item, parent, false)
+
+        //LayoutInflater -> class responsible to turn xml layoutsinto view object
+        // parent - here it is Recycleview
+
        return ExampleViewHolder(itemView)
+        //return
 
 
 //        val layoutInflater: LayoutInflater = LayoutInflater.from(parent.context)       //setting the layout
@@ -25,19 +33,25 @@ class ExampleAdapter (private val exampleList: List<ExampleItem>) : RecyclerView
 
     override fun onBindViewHolder(holder: ExampleViewHolder, position: Int) {
 
+
         val currentItem = exampleList[position]
 
        holder.imageView.setImageResource(currentItem.imageResource)
        holder.textView1.text = currentItem.text1
        holder.textView2.text = currentItem.text2
 
+       // if (position == 0) {
+        //    holder.textView1.setBackgroundColor(color.YELLOW);
+       // }
+
     }
 
-    override fun getItemCount(): Int {
+    override fun getItemCount(): Int {  //to know how many items will be there in RecyclerView
         return exampleList.size
     }
 
     class ExampleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        //this class is given as argument for RecycleView.Adapter
         val imageView: ImageView = itemView.findViewById(R.id.image_view)
         val textView1: TextView = itemView.findViewById(R.id.text_view_1)
         val textView2: TextView = itemView.findViewById(R.id.text_view_2)
--- app/src/main/java/com/example/recyclerviewtrial/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewtrial/MainActivity.kt
@@ -2,24 +2,52 @@ package com.example.recyclerviewtrial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+   private val exampleList = generateDummyList(500)
+    private val adapter = ExampleAdapter(exampleList)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val exampleList = generateDummyList(500)
+
 
         val recycler_view = findViewById<RecyclerView>(R.id.recycler_view)
 
-        recycler_view.adapter = ExampleAdapter(exampleList)
-        recycler_view.layoutManager = LinearLayoutManager(this)
-        recycler_view.setHasFixedSize(true)
+        recycler_view.adapter = adapter
+        recycler_view.layoutManager = LinearLayoutManager(this) //positioning the elements into list item- Linear/Grid
+        recycler_view.setHasFixedSize(true)         //for performance /optimization when we know the size of list
     }
 
-    private fun generateDummyList(size: Int): List<ExampleItem> {
+    fun insertItem(view: View) {
+        val index = Random.nextInt(8)
+
+        val newItem = ExampleItem(
+            R.drawable.ic_baseline_adb,
+            text1 = "New Item at position $index",
+            text2 = "Line 2"
+        )
+        exampleList.add(index, newItem)
+        adapter.notifyItemInserted(index)       //affects
+
+    }
+
+    fun removeItem(view: View) {
+        val index = Random.nextInt(8)
+
+        exampleList.removeAt(index)
+        adapter.notifyItemRemoved(index)
+    }
+
+
+    private fun generateDummyList(size: Int): ArrayList<ExampleItem> {       // a fn to create random data
+        //ArrayList = read and wirte
+        //List - read only
 
         val list = ArrayList<ExampleItem>()
 
@@ -30,7 +58,7 @@ class MainActivity : AppCompatActivity() {
                 else -> R.drawable.ic_baseline_adb
             }
 
-            val item = ExampleItem(drawable, "Item $i", "Line 2")
+            val item = ExampleItem(drawable, "Item $i", "Line 2") //constructor is called here
             list.add(item)
         }
 
